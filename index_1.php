4ca7eda91ae36c2e9c70ef3047378a229f8185f3
tsanchezl <tomassanchezlara@gmail.com>
2015-11-04 17:58:19 +0100

    pujada exercicis

---
+++ index_1.php
@@ -0,0 +1,70 @@
+<!DOCTYPE html>
+<html>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+<body>
+<h1> Test page PHP </h1>
+<?php 
+
+//VAR definitions"
+$x = "hello my name is Thomas,";
+$y = "Whats your name?";
+define("WELLCOME","<h2>Missatge de benvinguda</h2><br>");
+//$cars = array("seat","bmw","mercedes");
+
+
+
+//FUNCTIONS
+function newline() {
+echo "<br>";
+}
+
+function list_cars() {
+	$cars = array("seat","bmw","mercedes");
+	var_dump($cars);
+}
+
+echo WELLCOME;
+
+list_cars();
+class cotxe {
+
+	function cotxe (){
+
+	$this->model="citroen";
+
+	}
+}
+
+// creació d'un objecte tipus cotxe
+$bibi = new cotxe();
+// es mostra la propietat model de l'objecte cotxe
+newline();
+echo "$bibi->model";
+// Indica el tamany del string
+newline();
+echo "la frase $x medeix:",strlen($x);
+newline();
+echo "la frase \"$y\" té:",str_word_count($y)," paraules";
+newline();
+echo "la frase \"$x\" al revés:",strrev($x);
+newline();
+$match=strpos("$x","Thomas");
+echo $match;
+newline();
+echo str_replace("Thomas","Sanchez",$x);
+
+$t = date("d");
+newline();
+echo "t val:",$t;
+newline();
+if ($t < "20") {
+    echo "Have a good day!";
+} else {
+    echo "Have a good night!";
+}
+ ?>
+
+<h1> End Test page </h1>
+
+</body>
+</html>
---
+++ index_2.php
@@ -0,0 +1,25 @@
+<html>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+<body>
+<?php
+
+echo "<h1>Exercicis de bucles</h1>";
+echo "<hr>";
+$color="blue";
+echo "<br>";
+
+switch ($color){
+	case "red":
+		echo "el color es vermell";
+		break;
+	case "blue":
+		echo "el color es blau";
+		break;
+	default:
+		echo "el color no es ni blau ni vermell";
+		break;
+}
+
+?>
+</body>
+</html>
